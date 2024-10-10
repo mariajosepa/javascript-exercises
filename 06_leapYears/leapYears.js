c710f595a17c7f39b7eb20cedca8803e6b48371f
mariajose <mariamariapava123@gmail.com>
2024-10-10 17:58:30 -0500

    [NEW] Completed exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    let isLeapYear = false
+    if (year % 4 === 0) {
+        if(year % 100 === 0){
+            if (year % 400 === 0) {
+                isLeapYear = true;
+            }
+        }else{
+            isLeapYear = true
+        }
+    }
+    return isLeapYear
 };
+
+console.log(leapYears(700));
 
 // Do not edit below this line
 module.exports = leapYears;

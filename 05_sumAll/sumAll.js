bfba8e4d22a9b37e21f273d420dbdf24a65b5a37
mariajose <mariamariapava123@gmail.com>
2024-10-10 17:45:58 -0500

    [NEW] Completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(low,hi) {
+
+    let sum = 0;
+    let ans = 0;
+
+    if (low > 0 && Number.isInteger(low) && Number.isInteger(hi)){
+
+        if (low > hi){
+            let temp = hi;
+            hi = low;
+            low = temp;
+        }
+
+        for (let i = low; i < hi+1; i++) {
+            sum = sum + i;
+            }
+        ans = sum;
+    }
+    else{
+        ans = 'ERROR'
+    }
+
+    return ans
 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;

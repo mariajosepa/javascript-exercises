9fe7b926e03538a9d554896d82889a9a056721b7
mariajose <mariamariapava123@gmail.com>
2024-10-10 12:56:51 -0500

    [NEW] Completed exercises 01,02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word,repeatAmount) {
+  let count = 0;
+  let result = '';
+  if (repeatAmount >= 0) {
+    while (count < repeatAmount) {
 
+      result = result + word;
+      count +=1;
+    }
+  }else{
+    result = 'ERROR';
+  }
+ 
+  return result
 };
 
 // Do not edit below this line

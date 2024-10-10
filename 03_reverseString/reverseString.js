e2a99b8db5a6c5498d959c3f29f0b5baa2105efd
mariajose <mariamariapava123@gmail.com>
2024-10-10 13:12:53 -0500

    [NEW] Completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let result = '';
+    const charsArray = str.split('');
+    charsArray.reverse()
+    result = charsArray.join('');
 
+    return result
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
